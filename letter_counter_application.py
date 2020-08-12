6cc20459b1b67ef7c70a55ad0adcc5b7a894bdce
prachi0011 <60473704+prachi0011@users.noreply.github.com>
2020-08-12 21:43:35 +0530

    Add files via upload

---
+++ letter_counter_application.py
@@ -0,0 +1,8 @@
+#letter counter application
+print("Welcome to Letter Counter Application")
+name = input("Enter Your name : ")
+string = "Don’t be intimidated by what you don’t know. That can be your greatest strength and ensure that you do things differently from everyone else."
+print(string)
+character = input("Enter character : ")
+print("letter repeat ", string.count(character), "times in above string")
+                  
